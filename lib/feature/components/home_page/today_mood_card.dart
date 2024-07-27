5eb731d9c160ef9b73a2ff9d46bd31d4716fafa2
zuhalal <zuhalalimulhadi19@gmail.com>
2024-07-27 20:19:31 +0700

    fix: timestamp in mood card

--- lib/feature/components/home_page/today_mood_card.dart
+++ lib/feature/components/home_page/today_mood_card.dart
@@ -40,16 +40,19 @@ class _TodayMoodCardState extends State<TodayMoodCard> {
                 children: [
                   Column(
                     children: [
+                      Container(
+                        alignment: Alignment.centerLeft,
+                        child: Text(
+                          "15:00",
+                          style: GoogleFonts.inter(fontSize: 12, fontWeight: FontWeight.w400),
+                          textAlign: TextAlign.left,
+                        ),
+                      ),
                       Image.network(
                           'https://cdn.discordapp.com/attachments/784066636935331860/1266734084118286409/Component_1.png?ex=66a6398c&is=66a4e80c&hm=ab8e5b9f228ed165482013f312f274392856560ac55af03282b69482eaf8c41a&'),
                       Text(
                         widget.mood,
                         style: GoogleFonts.inter(fontSize: 12, fontWeight: FontWeight.w500),
-                        textAlign: TextAlign.left,
-                      ),
-                      Text(
-                        "15:00",
-                        style: GoogleFonts.inter(fontSize: 12, fontWeight: FontWeight.w400),
                         textAlign: TextAlign.left,
                       ),
                     ],
